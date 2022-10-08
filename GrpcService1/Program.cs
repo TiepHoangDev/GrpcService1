34050ea3276d33b18dbbaf91e44cd810ce59e0cd
WIN10-E <tiephoang.dev@gmail.com>
2022-10-08 11:31:42 +0700

    Add project files.

---
+++ GrpcService1/Program.cs
@@ -0,0 +1,27 @@
+using GrpcService1.Services;
+using Microsoft.Extensions.Hosting.WindowsServices;
+
+var options = new WebApplicationOptions
+{
+    ContentRootPath = WindowsServiceHelpers.IsWindowsService() ? AppContext.BaseDirectory : default,
+    Args = args
+};
+
+var builder = WebApplication.CreateBuilder(options);
+
+// Additional configuration is required to successfully run gRPC on macOS.
+// For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
+
+builder.WebHost.UseUrls("http://localhost:12345");
+
+// Add services to the container.
+builder.Services.AddGrpc();
+builder.Host.UseWindowsService();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.MapGrpcService<GreeterService>();
+app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+
+await app.RunAsync();
